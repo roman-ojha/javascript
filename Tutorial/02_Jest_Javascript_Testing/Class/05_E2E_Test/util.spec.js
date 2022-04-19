0c0cf809a8c09e3e55e6a8f891333ca00d6a3aca
Roman Ojha <razzroman98@gmail.com>
2022-04-19 10:16:36 +0545

    E2E testing using puppeteer

--- Tutorial/02_Jest_Javascript_Testing/Class/05_E2E_Test/util.spec.js
+++ Tutorial/02_Jest_Javascript_Testing/Class/05_E2E_Test/util.spec.js
@@ -1,30 +1,4 @@
-const { generateText, checkAndGenerate } = require("./util");
-// because we want to test 'generateText' function we will going to import inside here
-const puppeteer = require("puppeteer");
-// including puppeteer for e2e test
-
-// Unit test:
-
-test("should output name and age", () => {
-  const text = generateText("Max", 29);
-  //   now here we are calling function here and passing the required argument
-  expect(text).toBe("Max (29 years old)");
-  const text2 = generateText("Anna", 28);
-  expect(text2).toBe("Anna (28 years old)");
-});
-
-test("should output data-less text", () => {
-  const text = generateText("", null);
-  expect(text).toBe(" (null years old)");
-});
-
-// Integration test:
-test("should generate a valid text output", () => {
-  const text = checkAndGenerate("Max", 29);
-  // here check and Generate is the function that again require the another function inside
-  expect(text).toBe("Max (29 years old)");
-});
-
+const { generateText, checkAndGenerate } = require("../../util");
 // E2E Test
 
 test("should create an element with correct class", async () => {
