178871c0f1e1c9e90e9211a5f9c9c579950549ce
Roman-Ojha <razzroman98@gmail.com>
2022-09-07 13:30:11 +0545

    refactor: added jest as submodule

--- Tutorial/02_Jest_Javascript_Testing/Class/06_Mocking_Async_Code/__mocks__/http.js
+++
@@ -1,6 +0,0 @@
-const fetchData = () => {
-  return Promise.resolve({ title: "delectus aut autem" });
-  //   now here we are returning the static value so that while testing we don't want to depend on other other third party
-};
-
-exports.fetchData = fetchData;
--- Tutorial/02_Jest_Javascript_Testing/Class/06_Mocking_Async_Code/app.js
+++
@@ -1,5 +0,0 @@
-const { printTitle } = require("./util");
-
-const button = document.querySelector("button");
-
-button.addEventListener("click", printTitle);
