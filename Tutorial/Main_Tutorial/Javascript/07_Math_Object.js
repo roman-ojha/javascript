10e1d9ea93c71a89a929814f5892b6110749f756
Roman-Ojha <razzroman98@gmail.com>
2023-02-22 17:45:28 +0545

    docs: about math object & it's methods

---
+++ Tutorial/Main_Tutorial/Javascript/07_Math_Object.js
@@ -0,0 +1,42 @@
+/*
+    *) Math Object:
+        -> The Math object allows we to perform mathematical tasks. 
+        -> The Math object includes several mathematical constants and methods.
+        -> Note: Math is not a constructor. All properties and methods of Math can be called by using Math as an object without creating it.
+
+    *) Properties:
+        • Math.E:
+            -> Returns Euler's number (approx. 2.718)
+        • Math.LN2:
+            -> Returns the natural logarithm of 2 (approx. 0.693)
+        • Math.LN10:
+            -> Returns the natural logarithm of 10 (approx. 2.302)
+        • Math.LOG2E:
+            -> Returns the base-2 logarithm of E (approx. 1.442)
+        • Math.LOG10E:
+            -> Returns the base-10 logarithm of E (approx. 0.434)
+        • Math.PI:
+            -> Returns PI (approx. 3.14159)
+        • Math.SQRT1_2:
+            -> Returns the square root of 1/2 (approx. 0.707)
+        • Math.SQRT2:
+            -> Returns the square root of 2 (approx. 1.414)
+
+    *) Methods:
+        -> abs(x): Returns the absolute value of x
+        -> ceil(x): Returns x, rounded upwards to the nearest integer
+        -> floor(x): Returns x, rounded downwards to the nearest integer
+        -> log(x): Returns the natural logarithm (base e) of x
+        -> max(x,y,z,...,n): Returns the number with the highest value
+        -> min(x,y,z,...,n): Returns the number with the lowest value
+        -> pow(x,y): Returns the value of x to the power of y
+        -> sqrt(x): Returns the square root of x
+        -> random( ): Returns a random number between 0 and 1
+        -> round(x): Rounds x to the nearest integer
+        -> sin(x): Returns the sine of x (x is in radians)
+        -> cos(x): Returns the cosine of x (x is in radians)
+        -> tan(x): Returns the tangent of an angle
+*/
+
+var pi_value = Math.PI;
+var sqrt_value = Math.sqrt(16);
